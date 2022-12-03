3e49e0563c858d97b6c82451fddb23c83d2aead2
42corozco <42corozco@gmail.com>
2022-12-03 19:50:17 +0100

    A8 normed ok

--- A8/main.c
+++ A8/main.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0 OR MIT
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -5,43 +6,45 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-// Dont have a license, LOL
-MODULE_LICENSE("LICENSE");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
 static ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs);
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
 
-static struct file_operations myfd_fops = {
-	.owner = THIS_MODULE, .read = &myfd_read, .write = &myfd_write
-};
+static const struct file_operations myfd_fops = {
+	.owner = THIS_MODULE,
+	.read = &myfd_read,
+	.write = &myfd_write };
 
 static struct miscdevice myfd_device = {
-	.minor = MISC_DYNAMIC_MINOR,.name = "reverse",
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "reverse",
 	.fops = &myfd_fops };
 
 char str[PAGE_SIZE];
-char *tmp;
+
 static int __init myfd_init(void)
 {
 	int retval;
-	retval = misc_register(&(*(&(myfd_device))));
-	return 1;
+
+	retval = misc_register(&myfd_device);
+	return retval;
 }
 
 static void __exit myfd_cleanup(void)
 {
+	misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
-	size_t i t;
+	size_t i, t;
 	size_t res;
 	char *tmp;
-	/***************
-	* Malloc like a boss
-	***************/
+
+	/**************** Malloc like a boss***************/
 	tmp = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
 	t = strlen(str) - 1;
 	for (i = 0; t > i; i++)
@@ -55,9 +58,10 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
 {
 	ssize_t res;
+
 	res = 0;
 	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-	// 0x0 = ’\0’
+	// 0x0 = "\0"
 	str[size + 1] = 0x0;
 	return res;
 }
