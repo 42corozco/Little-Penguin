c42a61296d126688fc92cb4d7843af43d4ed1d6b
42corozco <42corozco@gmail.com>
2022-12-03 18:44:46 +0100

    no funciona aun

--- A8/main.c
+++ A8/main.c
@@ -36,35 +36,20 @@ static void __exit myfd_cleanup(void)
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
-	size_t i;
-	char *tmp;
-	ssize_t res;
-
-	/**************** Malloc like a boss***************/
-	tmp = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
-	for (i = 0; i < strlen(str); i++)
-		tmp[i] = str[strlen(str) - i - 1];
-	tmp[i] = 0x0;
-
-	res = simple_read_from_buffer(user, size, offs, tmp, i);
-	kfree(tmp);
-	return res;
-}
-
-ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
-{
 	size_t i t;
+	size_t res;
 	char *tmp;
 	/***************
 	* Malloc like a boss
 	***************/
 	tmp = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
 	t = strlen(str) - 1;
-	for (i = 0; t > i; i++) {
-		tmp[i] = str[t-- - i];
-	}
+	for (i = 0; t > i; i++)
+		tmp[i] = str[t - i];
 	tmp[i] = 0x0;
-	return simple_read_from_buffer(user, size, offs, tmp, i);
+	res =  simple_read_from_buffer(user, size, offs, tmp, i);
+	kfree(tmp);
+	return res;
 }
 
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
