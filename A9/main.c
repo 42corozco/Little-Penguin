ff4b8155aa7121b84ff0aa11815784e98b0b0bc1
42corozco <42corozco@gmail.com>
2022-12-16 18:46:34 +0100

    ok

--- A9/main.c
+++ A9/main.c
@@ -2,13 +2,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
-#include <asm/uaccess.h>
 
-#include <linux/string.h>
-#include <linux/mount.h>
 #include <linux/fs_struct.h>
-#include <linux/dcache.h>
-#include <linux/seq_file.h>
 
 #define procfs_name "mymounts"
 static struct proc_dir_entry *proc_file;
@@ -19,10 +14,10 @@ ssize_t read_proc(struct file *filp, char *buf, size_t len, loff_t *offp )
 
     list_for_each_entry(curdentry, &current->fs->root.mnt->mnt_root->d_subdirs, d_child)
     {
-        //if ( curdentry->d_flags & DCACHE_MOUNTED)
-            printk("%s is mounted", curdentry->d_name.name);
+        if ( curdentry->d_flags & DCACHE_MOUNTED)
+            pr_info("%s is mounted", curdentry->d_name.name);
     }
-    return -1;
+    return 0;
 }
 
 static struct file_operations myops =
@@ -33,7 +28,7 @@ static struct file_operations myops =
 
 static int __init proc_init(void)
 {
-	proc_file = proc_create(procfs_name, 0644, NULL,(const struct proc_ops *)&myops);
+	proc_file = proc_create(procfs_name, 0444, NULL,(const struct proc_ops *)&myops);
 	if (proc_file == NULL) {
 		remove_proc_entry(procfs_name, NULL);
 		pr_info("Error: could not initialize /proc/%s\n", procfs_name);
@@ -52,5 +47,5 @@ module_init(proc_init);
 module_exit(proc_exit);
 
 MODULE_AUTHOR("corozco");
-MODULE_DESCRIPTION("A8");
+MODULE_DESCRIPTION("A9");
 MODULE_LICENSE("GPL");
